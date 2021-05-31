d0b8ee414d6cdf27881ee84da65459375b6b725e
mnmari <mariananunoli@gmail.com>
2021-05-31 00:15:56 -0300

    Initial commit

---
+++ lift-mno/app/src/main/java/com/mariana/lift_mno/Lift.kt
@@ -0,0 +1,37 @@
+package com.mariana.lift_mno
+
+data class Lift(private var currentNumberOfPeople: Int,
+                private val maxNumberOfPeople: Int,
+                private var currentFloor: Int,
+                private val numberOfFloors: Int){
+
+    fun getCurrentPeopleInLift() : Int = currentNumberOfPeople
+    fun getMaxNumberOfPeople() : Int = maxNumberOfPeople
+    fun showCurrentPeopleInLift() : String = "Há $currentNumberOfPeople pessoa(s) no elevador!"
+
+    fun updateCurrentFloor(floor: Int){
+        if (floor in 0..numberOfFloors)
+            currentFloor = floor
+    }
+
+    fun updateNumberOfPeople(option: Int) {
+
+        if (option == 1 && currentNumberOfPeople < maxNumberOfPeople)
+            currentNumberOfPeople += 1
+
+        else if (option == 2 && currentNumberOfPeople > 0)
+            currentNumberOfPeople -= 1
+    }
+
+    fun validateCurrentFloor(floor: Int) : String {
+
+        return if (floor < 0 || floor > numberOfFloors)
+            "Insira um andar válido!"
+
+        else if (floor == 0)
+            "O elevador agora se encontra no térreo!"
+
+        else
+            "O elevador agora se encontra no $floor andar!"
+    }
+}
---
+++ lift-mno/app/src/main/java/com/mariana/lift_mno/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.mariana.lift_mno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var txtLocation2 : TextView
+    lateinit var txtLocation3 : TextView
+
+    lateinit var txtCurrentPeopleInLift : TextView
+    lateinit var txtTotalPeopleInLift : TextView
+
+    var edtInputText : EditText? = null
+    lateinit var btnSetFloor : Button
+    lateinit var btnIn : Button
+    lateinit var btnOut : Button
+
+    private val maxNumberOfPeople = 5 // fixo
+    private val numberOfFloors = 10 // fixo
+    private var numberOfPeople = 0
+    private val startFloor = 0
+    private var selectedFloor = 0
+
+    var lift = Lift(numberOfPeople, maxNumberOfPeople, startFloor, numberOfFloors)
+    var option = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+
+        updateFloorViewTextView(selectedFloor, numberOfFloors)
+        updateNumberOfPeopleTextView()
+
+        btnSetFloor.setOnClickListener(::onClickBtnSetFloor)
+
+        btnIn.setOnClickListener(::onClickBtnIn)
+
+        btnOut.setOnClickListener(::onClickBtnOut)
+    }
+
+    private fun bindViews(){
+        txtLocation2 = findViewById(R.id.txtLocation2)
+        txtLocation3 = findViewById(R.id.txtLocation3)
+
+        txtCurrentPeopleInLift = findViewById(R.id.txtCurrentPeopleInLift)
+        txtTotalPeopleInLift = findViewById(R.id.txtTotalPeopleInLift)
+
+        edtInputText = findViewById(R.id.edtInputText)
+        btnSetFloor = findViewById(R.id.btnSetFloor)
+        btnIn = findViewById(R.id.btnIn)
+        btnOut = findViewById(R.id.btnOut)
+    }
+
+    private fun onClickBtnIn(view: View) {
+        option = 1
+        lift.updateNumberOfPeople(option)
+        numberOfPeople = lift.getCurrentPeopleInLift()
+        updateNumberOfPeopleTextView()
+        Toast.makeText(this, lift.showCurrentPeopleInLift(), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onClickBtnOut(view: View) {
+        option = 2
+        lift.updateNumberOfPeople(option)
+        numberOfPeople = lift.getCurrentPeopleInLift()
+        updateNumberOfPeopleTextView()
+        Toast.makeText(this, lift.showCurrentPeopleInLift(), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onClickBtnSetFloor(view: View) {
+        selectedFloor = edtInputText?.text.toString().toIntOrNull()!!
+
+        updateFloorViewTextView(selectedFloor, numberOfFloors)
+        lift.updateCurrentFloor(selectedFloor)
+        Toast.makeText(this, lift.validateCurrentFloor(selectedFloor), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updateNumberOfPeopleTextView() {
+        txtCurrentPeopleInLift.text = lift.getCurrentPeopleInLift().toString()
+        txtTotalPeopleInLift.text = lift.getMaxNumberOfPeople().toString()
+    }
+
+    private fun updateFloorViewTextView(selectedFloor: Int, numberOfFloors: Int) {
+        selectedFloor.let {
+            if (selectedFloor in 1..numberOfFloors) {
+                txtLocation2.text = selectedFloor.toString() + "°"
+                txtLocation3.text = "andar"
+            } else if (selectedFloor == 0) {
+                txtLocation2.text = "Térreo"
+                txtLocation3.text = ""
+            }
+        }
+    }
+}
